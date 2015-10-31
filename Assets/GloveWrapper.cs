aa1e15fe3b3f44f9a428fa5b5dca05be70115a01
Mika Hämäläinen <mika.hamalainen@helsinki.fi>
2015-10-31 14:40:17 +0200

    started bluetooth

---
+++ Assets/GloveWrapper.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class GloveWrapper : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		AndroidJavaClass cls = new AndroidJavaClass("com.aau.bluetooth.cultar.glove.android.Glove");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
