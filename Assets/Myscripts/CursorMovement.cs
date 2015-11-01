3b93b221c24c05f82b661eb71419590d0f856ece
Dark <aprilgowk@gmail.com>
2015-11-01 18:57:34 +0200

    cursor can move by keyboard
    for example, ‘w’,’a’,’s’,’d’ can control the direction
    ‘up’,’left’,’down’,’right’ of cursor

---
+++ Assets/Myscripts/CursorMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorMovement : MonoBehaviour {
+
+	public float moveSpeed;
+	public void moveTo (float x, float y){
+		transform.Translate (moveSpeed * x * Time.deltaTime, 
+		                     moveSpeed * y * Time.deltaTime,
+		                     0f);
+	}//This is for the input source that can be changed to the glove.
+
+	// Use this for initialization
+	void Start () {
+		moveSpeed = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveTo (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+	}
+}
