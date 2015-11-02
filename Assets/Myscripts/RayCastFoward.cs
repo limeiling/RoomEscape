7d9a3fdc08e311b35cd9609b068bd66ebed032dd
Mika Hämäläinen <mika.hamalainen@helsinki.fi>
2015-11-02 21:42:47 +0200

    some fixes

--- Assets/Myscripts/RayCastFoward.cs
+++ Assets/Myscripts/RayCastFoward.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class RayCastFoward : MonoBehaviour {
 
-	float distance;
+	
 
 	// Use this for initialization
 	void Start () {
@@ -12,23 +12,14 @@ public class RayCastFoward : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// cursor's position
-		GameObject cursorForHere = GameObject.Find ("Cursor");
-		Transform cursorPosition = cursorForHere.transform;
-		Vector3 cPos = cursorPosition.position;
 
 		// create a ray cast that orignates from the main camera position to cursor position
-		Vector3 forward = transform.TransformDirection (Vector3.forward) * 10;
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
 		RaycastHit hit;
-
 		Debug.DrawRay (transform.position,forward, Color.red);
-
-		if (Input.GetMouseButtonDown (0)) {
-			if(Physics.Raycast(transform.position,(forward),out hit)){
-				distance = hit.distance;
+			if(Physics.Raycast(transform.position,forward, out hit)){
 				print("Camera's Direction "+transform.position.x+" "+transform.position.y+" "+transform.position.z+" "
-				      + "Object's distance "+distance + " "+ hit.collider.gameObject.name);
+				      + "Object's distance" + hit.collider.gameObject.name);
 			}
-		}
 	}
 }
