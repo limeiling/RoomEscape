899be2263be2302dd99bdf8e46925546a2f82157
Dark <aprilgowk@gmail.com>
2015-11-03 15:40:43 +0200

    cursor can rotate with camera
    The ray from cursor can cast at correct objects that we want. Because
    the cursor move with rotation and always face to the direction of
    camera.

---
+++ Assets/Myscripts/CursorRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorRotation : MonoBehaviour {
+
+	public GameObject cursorFaceDirection;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//directionToFace = destination - source;
+		Vector3 directionToFace = cursorFaceDirection.transform.position - transform.position;
+
+		transform.rotation = Quaternion.LookRotation (-directionToFace);
+	}
+}
--- Assets/Myscripts/RayCastFoward.cs
+++ Assets/Myscripts/RayCastFoward.cs
@@ -13,13 +13,13 @@ public class RayCastFoward : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		// create a ray cast that orignates from the main camera position to cursor position
-		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		// create a ray cast that orignates from the cursor position
+		Vector3 forward = transform.TransformDirection(Vector3.forward) * 5;
 		RaycastHit hit;
 		Debug.DrawRay (transform.position,forward, Color.red);
 			if(Physics.Raycast(transform.position,forward, out hit)){
-				print("Camera's Direction "+transform.position.x+" "+transform.position.y+" "+transform.position.z+" "
-				      + "Object's distance" + hit.collider.gameObject.name);
+				print("Cursor's Direction "+transform.position.x+" "+transform.position.y+" "+transform.position.z+" "
+				     + hit.collider.gameObject.name);
 			}
 	}
 }
